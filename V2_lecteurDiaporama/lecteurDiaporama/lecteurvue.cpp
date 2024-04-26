ae24d9b8d3b56ffb4f6ddfeb3e37ce9d7285a1df
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2024-04-26 21:56:35 +0200

    Ajout de l'action de changement Vit. Dfl. + connections des autres btns & actions

--- V2_lecteurDiaporama/lecteurDiaporama/lecteurvue.cpp
+++ V2_lecteurDiaporama/lecteurDiaporama/lecteurvue.cpp
@@ -1,14 +1,70 @@
 #include "lecteurvue.h"
 #include "ui_lecteurvue.h"
 
+/************************
+ * CORPS DE LA CLASSE
+ ***********************/
+
+
 lecteurvue::lecteurvue(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::lecteurvue)
 {
     ui->setupUi(this);
+
+    // Connections
+    QObject::connect(ui->btnLancerDiapo, SIGNAL(clicked()), this, SLOT(demanderLancementDiapo()));
+    QObject::connect(ui->btnSuiv, SIGNAL(clicked()), this, SLOT(demanderAvancer()));
+    QObject::connect(ui->btnPrec, SIGNAL(clicked()), this, SLOT(demanderReculer()));
+    QObject::connect(ui->actionCharger_Diaporama, SIGNAL(triggered()), this, SLOT(demanderChangementDiaporama()));
+    QObject::connect(ui->btnArreterDiapo, SIGNAL(clicked()), this, SLOT(demanderArreterDiapo()));
+    QObject::connect(ui->actionChanger_Vitesse_Defilement, SIGNAL(triggered()), this, SLOT(demanderChangementVitesseDefilement()));
+    QObject::connect(ui->actionQuitter, SIGNAL(triggered()), this, SLOT(demanderFermetureLecteur()));
+    QObject::connect(ui->actionA_propos_de, SIGNAL(triggered()), this, SLOT(demanderInformations()));
 }
 
 lecteurvue::~lecteurvue()
 {
     delete ui;
+}
+
+
+
+
+/************************
+ *        SLOTS         *
+ ***********************/
+void lecteurvue::demanderAvancer() {
+    qDebug() << "Demande d'avancement dans le diaporama";
+}
+
+void lecteurvue::demanderReculer() {
+    qDebug() << "Demande de recul dans le diaporama";
+}
+
+void lecteurvue::demanderChangementVitesseDefilement() {
+    qDebug() << "Demande de changement de vitesse de défilement du diaporama";
+}
+
+void lecteurvue::demanderChangementDiaporama() {
+    qDebug() << "Demande de chargement de diaporama";
+}
+
+void lecteurvue::demanderArreterDiapo() {
+    qDebug() << "Demande d'arrêt de la diapositive en cours";
+}
+
+void lecteurvue::demanderLancementDiapo() {
+    qDebug() << "Demande de lancement d'une diapositive";
+}
+
+void lecteurvue::demanderFermetureLecteur() {
+    qDebug() << "Demande de fermeture du lecteur";
+    /* Fermeture réelle
+    close();
+    */
+}
+
+void lecteurvue::demanderInformations() {
+    qDebug() << "Demande d'informations";
 }
--- V2_lecteurDiaporama/lecteurDiaporama/lecteurvue.h
+++ V2_lecteurDiaporama/lecteurDiaporama/lecteurvue.h
@@ -14,10 +14,26 @@ class lecteurvue : public QMainWindow
     Q_OBJECT
 
 public:
+    // Constructeur & Destructeur
     lecteurvue(QWidget *parent = nullptr);
     ~lecteurvue();
 
 private:
+    // Design de la fenêtre
     Ui::lecteurvue *ui;
+
+public slots:
+    // Pour faire le lien avec les diaporamas
+    void demanderAvancer();
+    void demanderReculer();
+    void demanderChangementVitesseDefilement();
+    void demanderArreterDiapo();
+
+    // Pour gérer les actions propres au lecteur
+    void demanderChangementDiaporama();
+    void demanderLancementDiapo();
+    void demanderFermetureLecteur();
+    void demanderInformations();
+
 };
 #endif // LECTEURVUE_H
