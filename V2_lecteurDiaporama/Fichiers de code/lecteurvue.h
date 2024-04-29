d59574ebf6414a4f62c9c350985a53d0f4207465
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2024-04-29 18:50:59 +0200

    Corrections mineures

--- V2_lecteurDiaporama/Fichiers de code/lecteurvue.h
+++ V2_lecteurDiaporama/Fichiers de code/lecteurvue.h
@@ -26,10 +26,7 @@ public slots:
     // Pour faire le lien avec les diaporamas
     void demanderAvancer();
     void demanderReculer();
-<<<<<<< HEAD
     void demanderChangementVitesseDefilement();
-=======
->>>>>>> 4a61c008d2df54e393f53ab6cc1cfffdacd379bc
     void demanderArreterDiapo();
 
     // Pour gérer les actions propres au lecteur
@@ -37,8 +34,6 @@ public slots:
     void demanderLancementDiapo();
     void demanderFermetureLecteur();
     void demanderInformations();
-<<<<<<< HEAD
-=======
     void demanderChangementModeAuto();
     void demanderChangementModeManuel();
 
@@ -50,7 +45,5 @@ public slots:
     void demanderChangementVitesseDefilementX1_5();
     void demanderChangementVitesseDefilementX1_75();
     void demanderChangementVitesseDefilementX2();
->>>>>>> 4a61c008d2df54e393f53ab6cc1cfffdacd379bc
-
 };
 #endif // LECTEURVUE_H
