7fa14e24829f87c82207666e810d493e10effeb1
Etienne Dumai <etienne.dumai@gmail.com>
2024-06-05 15:16:05 +0200

    Avancement sur la creation du diaporama

--- V7_MVP_lecteurDiaporama/Fichiers de code/creationdiaporama.h
+++ V7_MVP_lecteurDiaporama/Fichiers de code/creationdiaporama.h
@@ -1,10 +1,20 @@
 #ifndef CREATIONDIAPORAMA_H
 #define CREATIONDIAPORAMA_H
 
-class CreationDiaporama
+#include <QDialog>
+
+namespace Ui {
+class CreationDiaporama;
+}
+
+class CreationDiaporama : public QDialog
 {
+    Q_OBJECT
 public:
     CreationDiaporama();
+private:
+    Ui::CreationDiaporama *ui;
+
 };
 
 #endif // CREATIONDIAPORAMA_H
